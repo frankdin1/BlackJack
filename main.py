96a493de4cf8430898ad75dea256a75a138dcd40
frankdin <frankdin@yahoo.com>
2024-04-01 18:07:37 -0400

    Added tabs in front of the lines that show user and computer cards and their scores. Added code to give user the option to not take another card and to reveal final score.

--- main.py
+++ main.py
@@ -12,19 +12,22 @@ if play_game == 'y':
     player_cards.append(random.choice(cards))
     player_cards.append(random.choice(cards))
     player_score = player_cards[0] + player_cards[1]
-    print(f"{player_cards}, current score: {player_score}")
+    print(f"\tYour cards: {player_cards}, current score: {player_score}")
 
 
     #pick two random cards from the deck for the computer, but only reveal the computer's first card.
     computer_cards.append(random.choice(cards))
     computer_cards.append(random.choice(cards))
     computer_score = computer_cards[0] + computer_cards[1]
-    print(f"Computer's first card: {computer_cards[0]}")
+    print(f"\tComputer's first card: {computer_cards[0]}")
 
-while(player_score < 21):
+while (player_score < 21):
     pick_another_card = input("Type 'y' to pick another card or 'n' to pass: ")
     if pick_another_card == 'y':
         player_cards.append(random.choice(cards))
         player_score += player_cards[-1]
-        print(f"{player_cards}, current score: {player_score}")
-        print(f"Computer's first card: {computer_cards[0]}")
+        print(f"\tYour cards: {player_cards}, current score: {player_score}")
+        print(f"\tComputer's first card: {computer_cards[0]}")
+    elif pick_another_card == 'n':
+        print(f"\tYour final hand: {player_cards}, final score: {player_score}")
+        print(f"\tComputer's final hand: {computer_cards}, final score: {computer_score}")
