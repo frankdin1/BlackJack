18e9904adbb1681dcd31f4c0e8ae0f62b5410911
frankdin <frankdin@yahoo.com>
2024-04-01 17:45:01 -0400

    Added code to print user cards and their score and to print computer's first card.

--- main.py
+++ main.py
@@ -10,14 +10,16 @@ if play_game == 'y':
     #pick two random cards from the deck for the user and display them
     player_cards.append(random.choice(cards))
     player_cards.append(random.choice(cards))
-    print(player_cards)
+    print(f"{player_cards}, current score: {player_cards[0] + player_cards[1]}")
+
 
     #pick two random cards from the deck for the computer, but only reveal the computer's first card.
     computer_cards.append(random.choice(cards))
     computer_cards.append(random.choice(cards))
-    print(computer_cards)
+    print(f"Computer's first card: {computer_cards[0]}")
 
     pick_another_card = input("Type 'y' to pick another card or 'n' to pass: ")
     if pick_another_card == 'y':
         player_cards.append(random.choice(cards))
-        print(player_cards)
+        print(f"{player_cards}, current score: {player_cards[0] + player_cards[1] + player_cards[2]}")
+        print(f"Computer's first card: {computer_cards[0]}")
