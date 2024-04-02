5ea2b523c3080b1b9516c72de5c815716f08ab22
frankdin <frankdin@yahoo.com>
2024-04-02 01:35:25 -0400

    Created function to ask player if they want to play game. Created function to perform initial deal.

--- main.py
+++ main.py
@@ -2,30 +2,45 @@ import random
 
 cards = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10]
 player_cards = []
-computer_cards = []
+dealer_cards = []
 player_score = 0
-computer_score = 0
-play_game = input("Do you want to play a game of BlackJack? Type 'y' or 'n': ")
+dealer_score = 0
 
-if play_game == 'y':
-    #pick two random cards from the deck for the user and display them
+
+def initiate_game():
+    play_game = input("Do you want to play a game of BlackJack? Type 'y' or 'n': ").lower()
+    if play_game == 'y':
+        player_scores = initial_deal()
+        return player_scores
+
+
+# function to deal cards at beginning of the game
+def initial_deal():
+    # pick two random cards from the deck for the user and display them
     player_cards.append(random.choice(cards))
     player_cards.append(random.choice(cards))
-    player_score = player_cards[0] + player_cards[1]
-    print(f"\tYour cards: {player_cards}, current score: {player_score}")
+    p_score = player_cards[0] + player_cards[1]
+    print(f"\tYour cards: {player_cards}, current score: {p_score}")
+
+    # pick two random cards from the deck for the computer, but only reveal the computer's first card.
+    dealer_cards.append(random.choice(cards))
+    dealer_cards.append(random.choice(cards))
+    d_score = dealer_cards[0] + dealer_cards[1]
+    print(f"\tComputer's first card: {dealer_cards[0]}")
+
+    return p_score, d_score
 
 
-    #pick two random cards from the deck for the computer, but only reveal the computer's first card.
-    computer_cards.append(random.choice(cards))
-    computer_cards.append(random.choice(cards))
-    computer_score = computer_cards[0] + computer_cards[1]
-    print(f"\tComputer's first card: {computer_cards[0]}")
+# Store returned values in variables created to store player and computer scores
+scores = initiate_game()
+player_score += scores[0]
+dealer_score += scores[1]
 
-if player_score == 21 and computer_score != 21:
+if player_score == 21 and dealer_score != 21:
     print("You won a BlackJack.")
-elif player_score == 21 and computer_score == 21:
+elif player_score == 21 and dealer_score == 21:
     print("You and the Computer both scored BlackJack.")
-elif player_score < 21 and computer_score == 21:
+elif player_score < 21 and dealer_score == 21:
     print("You lost.")
 
 while (player_score < 21):
@@ -34,18 +49,18 @@ while (player_score < 21):
         player_cards.append(random.choice(cards))
         player_score += player_cards[-1]
         print(f"\tYour cards: {player_cards}, current score: {player_score}")
-        print(f"\tComputer's first card: {computer_cards[0]}")
+        print(f"\tComputer's first card: {dealer_cards[0]}")
     elif pick_another_card == 'n':
         print(f"\tYour final hand: {player_cards}, final score: {player_score}")
-        print(f"\tComputer's final hand: {computer_cards}, final score: {computer_score}")
-        if player_score < 21 and computer_score > player_score:
+        print(f"\tComputer's final hand: {dealer_cards}, final score: {dealer_score}")
+        if player_score < 21 and dealer_score > player_score:
             print("You lose")
-        elif player_score < 21 and player_score > computer_score:
+        elif player_score < 21 and player_score > dealer_score:
             print("You win")
-        elif player_score < 21 and computer_score == player_score:
+        elif player_score < 21 and dealer_score == player_score:
             print("It's a draw.")
         break
 if player_score > 21:
     print(f"\tYour final hand: {player_cards}, final score: {player_score}")
-    print(f"\tComputer's final hand: {computer_cards}, final score: {computer_score}")
+    print(f"\tComputer's final hand: {dealer_cards}, final score: {dealer_score}")
     print("You went over. You lose.")
